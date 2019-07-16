dbb0f404bb9f65e444f5bdeff7a26baeb98211c1
lindazhong <daejong2014@163.com>
2019-07-16 09:44:03 +0800

    modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 ### 豌豆拼
 > 重写了豌豆拼python硬件交互逻辑
+
+
+```python
+from wonderguy import Display, wb_tool, Control, Led
+import random
+# wb_tool.show_console()
+
+d = Display()
+c = Control()
+l = Led()
+
+for i in range(20):
+    d.print(1,1,i)
+    v = c.get_sw4()
+    l.set_rgb(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    print(v)
+```
---
+++ wonderguy/WBLed.py
@@ -0,0 +1,58 @@
+from .WBits import WBits
+
+def _format_str_type(x):
+    if isinstance(x, str):
+       x = str(x).replace('"', '\\"')
+       x = "\"" + x + "\""
+    return x
+
+class Led(WBits):
+    def __init__(self, index = 1):
+        WBits.__init__(self)
+        self.index = index
+
+    
+    def set_rgb(self, r, g, b):
+        """
+        设置彩灯颜色（r,g,b参数都设置为0时，关闭LED）
+
+        :param r: 红色：0~255
+        :param g: 绿色：0~255
+        :param b: 蓝色：0~255
+        """
+
+        
+        args = []    
+        args.append(str(r))
+        args.append(str(g))
+        args.append(str(b))
+        command = 'led{}.set_rgb({})'.format(self.index, ",".join(args))
+        self._set_command(command)
+
+    
+    def fade_to_rgb(self, r, g, b, time, block = None, step = None):
+        """
+        控制彩灯由当前颜色在指定时间渐变到目标颜色
+
+        :param r: 目标红色：0~255
+        :param g: 目标绿色：0~255
+        :param b: 目标蓝色：0~255
+        :param time: 渐变时间：0~60 s  变化到目标颜色所用的时间
+        :param block: 阻塞参数：  False: 不阻塞 True: 阻塞
+        :param step: 变化次数：  在渐变时间内经过多少次变化达到目标颜色
+        """
+
+        
+        args = []    
+        args.append(str(r))
+        args.append(str(g))
+        args.append(str(b))
+        args.append(str(time))
+        if block != None:
+            args.append(str(block))
+        if step != None:
+            args.append(str(step))
+        command = 'led{}.fade_to_rgb({})'.format(self.index, ",".join(args))
+        self._set_command(command)
+
+    
