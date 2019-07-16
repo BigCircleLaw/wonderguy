ab7c81b8b162eed79b3fd27f48408f0fa5a1da42
lindazhong <daejong2014@163.com>
2019-07-16 09:22:08 +0800

    add led module

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-from wonderguy import Display, wb_tool, Control
+from wonderguy import Display, wb_tool, Control, Led
+import random
 # wb_tool.show_console()
 
 d = Display()
 c = Control()
+l = Led()
 
-for i in range(5):
+for i in range(20):
     d.print(1,1,i)
     v = c.get_sw4()
+    l.set_rgb(random.randint(0,255),random.randint(0,255),random.randint(0,255))
     print(v)
--- wonderguy/core.py
+++ wonderguy/core.py
@@ -2,4 +2,5 @@
 
 from .WBDisplay import Display
 from .WBControl import Control
+from .WBLed import Led
 from .Tool import wb_tool
