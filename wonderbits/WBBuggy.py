def79cccdc94f0844e801c634b0621a316af0981
Big Circle <794557226@qq.com>
2019-09-04 18:41:44 +0800

    发布(5,6,9)； 更新几个模块文件。

--- wonderbits/WBBuggy.py
+++ wonderbits/WBBuggy.py
@@ -17,33 +17,35 @@ class Buggy(WBits):
         self._set_command(command)
 
     
-    def get_battery_value(self):
+    def run(self, left, right):
         """
-        
-        :rtype: int
-        """
+        设置左右两个电机的转动速度
 
-        command = 'buggy{}.get_battery_value()'.format(self.index)
-        value = self._get_command(command)
-        return eval(value) 
-        
-    def set_buzzer(self, frequency):
-        """
-        设置频率为0表示关闭蜂鸣器
-
-        :param frequency: 频率：0~20000 Hz
+        :param left: 转速：-100~100  符号表示转动方向，绝对值为转动速度
+        :param right: 转速：-100~100  符号表示转动方向，绝对值为转动速度
         """
 
         
         args = []    
-        args.append(str(frequency))
-        command = 'buggy{}.set_buzzer({})'.format(self.index, ",".join(args))
+        args.append(str(left))
+        args.append(str(right))
+        command = 'buggy{}.run({})'.format(self.index, ",".join(args))
+        self._set_command(command)
+
+    
+    def stop(self):
+        """
+        左右两个电机停止转动
+
+        """
+
+        command = 'buggy{}.stop()'.format(self.index)
         self._set_command(command)
 
     
     def set_led1(self, r, g, b):
         """
-        
+        设置led1灯颜色（r,g,b参数都设置为0时，关闭LED）
 
         :param r: 红色：0~255
         :param g: 绿色：0~255
@@ -61,7 +63,7 @@ class Buggy(WBits):
     
     def set_led2(self, r, g, b):
         """
-        
+        设置led2灯颜色（r,g,b参数都设置为0时，关闭LED）
 
         :param r: 红色：0~255
         :param g: 绿色：0~255
@@ -77,39 +79,105 @@ class Buggy(WBits):
         self._set_command(command)
 
     
-    def set_motors(self, speed_left, speed_right):
+    def set_buzzer(self, frequency):
         """
-        符号表示转动方向，绝对值为转动速度
+        设置蜂鸣器声音频率（Hz）设置频率为0表示关闭蜂鸣器
 
-        :param speed_left: 转速：-100~100  符号表示转动方向，绝对值为转动速度
-        :param speed_right: 转速：-100~100  符号表示转动方向，绝对值为转动速度
+        :param frequency: 频率：0~20000 Hz
         """
 
         
         args = []    
-        args.append(str(speed_left))
-        args.append(str(speed_right))
-        command = 'buggy{}.set_motors({})'.format(self.index, ",".join(args))
+        args.append(str(frequency))
+        command = 'buggy{}.set_buzzer({})'.format(self.index, ",".join(args))
         self._set_command(command)
 
     
-    def get_s1(self):
+    def get_distance(self):
         """
-        亮度值代表相对强度，值越大代表亮度越强
+        获取检测的距离值（cm）
         :rtype: float
         """
 
-        command = 'buggy{}.get_s1()'.format(self.index)
+        command = 'buggy{}.get_distance()'.format(self.index)
         value = self._get_command(command)
         return eval(value) 
         
-    def get_s2(self):
+    def is_line_detected(self, t = None):
         """
-        亮度值代表相对强度，值越大代表亮度越强
+        判断某个循迹传感器是否检测为黑
+        :rtype: bool
+        """
+
+        
+        args = []    
+        if t != None:
+            args.append(str(t))
+        command = 'buggy{}.is_line_detected({})'.format(self.index, ",".join(args))
+        value = self._get_command(command)
+        return eval(value) 
+        
+    def get_light_s1(self):
+        """
+        获取s1检测的亮度值，亮度值代表相对强度，值越大代表亮度越强
         :rtype: float
         """
 
-        command = 'buggy{}.get_s2()'.format(self.index)
+        command = 'buggy{}.get_light_s1()'.format(self.index)
+        value = self._get_command(command)
+        return eval(value) 
+        
+    def get_light_s2(self):
+        """
+        获取s2检测的亮度值，亮度值代表相对强度，值越大代表亮度越强
+        :rtype: float
+        """
+
+        command = 'buggy{}.get_light_s2()'.format(self.index)
+        value = self._get_command(command)
+        return eval(value) 
+        
+    def set_servo(self, angle):
+        """
+        设置舵机转动到指定角度使用此函数后舵机将拥有维持角度的扭矩，施加外力改变舵机的角度会很困难
+
+        :param angle: 角度：0~180
+        """
+
+        
+        args = []    
+        args.append(str(angle))
+        command = 'buggy{}.set_servo({})'.format(self.index, ",".join(args))
+        self._set_command(command)
+
+    
+    def calibrate_with_black(self):
+        """
+        校准循迹传感器的黑色值
+
+        """
+
+        command = 'buggy{}.calibrate_with_black()'.format(self.index)
+        self._set_command(command)
+
+    
+    def calibrate_with_white(self):
+        """
+        
+
+        """
+
+        command = 'buggy{}.calibrate_with_white()'.format(self.index)
+        self._set_command(command)
+
+    
+    def get_battery_value(self):
+        """
+        
+        :rtype: int
+        """
+
+        command = 'buggy{}.get_battery_value()'.format(self.index)
         value = self._get_command(command)
         return eval(value) 
         
@@ -133,52 +201,6 @@ class Buggy(WBits):
         value = self._get_command(command)
         return eval(value) 
         
-    def is_tracer_check_black(self, channel):
-        """
-        表示t1~t5
-        :rtype: bool
-        """
-
-        
-        args = []    
-        args.append(str(channel))
-        command = 'buggy{}.is_tracer_check_black({})'.format(self.index, ",".join(args))
-        value = self._get_command(command)
-        return eval(value) 
-        
-    def get_tracer_value(self, channel):
-        """
-        表示t1~τ5
-        :rtype: float
-        """
-
-        
-        args = []    
-        args.append(str(channel))
-        command = 'buggy{}.get_tracer_value({})'.format(self.index, ",".join(args))
-        value = self._get_command(command)
-        return eval(value) 
-        
-    def is_t6_check_unobstructed(self):
-        """
-        True：t6无遮挡False：t6被遮挡
-        :rtype: bool
-        """
-
-        command = 'buggy{}.is_t6_check_unobstructed()'.format(self.index)
-        value = self._get_command(command)
-        return eval(value) 
-        
-    def is_t7_check_unobstructed(self):
-        """
-        True：t7无遮挡False：t7被遮挡
-        :rtype: bool
-        """
-
-        command = 'buggy{}.is_t7_check_unobstructed()'.format(self.index)
-        value = self._get_command(command)
-        return eval(value) 
-        
     def set_calibration_percentage(self, value):
         """
         阈值=循迹传感器检测的黑色值*value%+循迹传感器检测的白色值*（1-value%）
@@ -193,25 +215,18 @@ class Buggy(WBits):
         self._set_command(command)
 
     
-    def calibration_black(self):
+    def get_tracer_value(self, channel):
         """
+        表示t1~τ5
+        :rtype: float
+        """
+
         
-
-        """
-
-        command = 'buggy{}.calibration_black()'.format(self.index)
-        self._set_command(command)
-
-    
-    def calibration_white(self):
-        """
+        args = []    
+        args.append(str(channel))
+        command = 'buggy{}.get_tracer_value({})'.format(self.index, ",".join(args))
+        value = self._get_command(command)
+        return eval(value) 
         
-
-        """
-
-        command = 'buggy{}.calibration_white()'.format(self.index)
-        self._set_command(command)
-
-    
 
     
